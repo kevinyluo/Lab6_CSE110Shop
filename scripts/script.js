a87bd74c36230462df855219c77e91ef4fb09007
Kevin Luo <kevinyluo@gmail.com>
2021-02-13 22:31:04 -0800

    fixed CSS bugs

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,31 @@
 // Script.js
 
+let myStorage = window.localStorage;
+
 window.addEventListener('DOMContentLoaded', () => {
-  // TODO
+  fetch("https://fakestoreapi.com/products")
+  .then(response => response.json())
+  .then((data) => {
+    // Store the data in local storage
+    let str = JSON.stringify(data);
+    localStorage.setItem("items", str);
+
+    // Add the items to product item container
+    const container = document.getElementById("product-list");
+    for(let i = 0; i < data.length; i++){
+      const product =  data[i]
+      // console.log(product.price)
+
+      const newProduct = document.createElement('product-item')
+      newProduct.setAttribute('title', product.title)
+      newProduct.setAttribute('price', product.price)
+      newProduct.setAttribute('src', product.image)
+      newProduct.setAttribute('id', product.id)
+
+      container.appendChild(newProduct)
+    }
+
+  })
 });
+
+
